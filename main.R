73ed3d368fb87a806bd5da14df6c1958d254a8ad
Ike-G <07iglassbrook@brightoncollege.net>
2021-12-22 23:11:36 +0000

    working on themes

--- main.R
+++ main.R
@@ -1,20 +1,3 @@
-# df <- transmute(
-#   seq(from = -pi, to = pi, by = 0.01) %>% expand.grid(x_i = ., y_i = .),
-#   x = runif(1, -1, 10) * x_i^2 - sin(y-i^2),
-#   y = runif(1, -1, 10) * y_i^3 - cos(x_i^2) * y_i^4
-# )
-# 
-# plot <- df %>%
-#   ggplot(aes(x=x, y=y)) +
-#   geom_point(alpha = 0.1, size = 0, shape = 20, color = "#c1a06e") +
-#   theme_void() +
-#   coord_fixed() +
-#   coord_polar() +
-#   theme(
-#     panel.background = element_rect(fill = "#1a3657"),
-#     plot.background = element_rect(fill = "#1a3657")
-#   )
-
 # Libraries ---------------------------------------------------------------
 
 library(ggplot2)
@@ -28,15 +11,13 @@ triangleRotation <- pi/6
 # circle packing radius
 pr <- 0.05
 # points per circle
-ppc <- 3 
+ppc <- 5 
 width <- 1.5 
 height <- 1.5 
 margin <- -0.2
 seed <- 127
-stepLength <- 0.01
-numSteps <- 100
-
-sprintf("Lines: %f", ppc*(width/sqrt(3) +height)/pr)
+stepLength <- 0.001
+numSteps <- 200
 
 # Helpers -----------------------------------------------------------------
 
@@ -55,43 +36,34 @@ genField <- function(points, seed = 1, stepLength = 0.01, numSteps = 20) {
   for (i in 1:numSteps) {
     o <- bind_rows(o, state)
     state <- mutate(state, 
-                    x=x+stepLength*cospi(2*gen_perlin(x,y,seed=seed)), 
-                    y=y+stepLength*sinpi(2*gen_perlin(x,y,seed=seed)), 
+                    g=gen_perlin(x,y,seed=seed),
+                    x=x+stepLength*cospi(2*g), 
+                    y=y+stepLength*sinpi(2*g), 
                     step=step+1)
   }
   print("Generated field")
   o
 }
 
-
-seq(0, 2*pi, 0.01) %>%
-  data.frame(x = ., y = tri((. + triangleRotation)%%(2*pi))) %>%
-  ggplot(aes(x = x, y = y)) +
-  coord_polar("x") +
-  xlim(0, 2*pi) + ylim(0, 1) +
-  geom_area()
-  
+theme_custom <- function(bg, ...) theme(
+  line = element_blank(), rect = element_blank(),
+  text = element_blank(), title = element_blank(),
+  axis.text = element_blank(), axis.title = element_blank(),
+  axis.ticks.length = unit(0, "pt"), axis.ticks.length.x = NULL,
+  axis.ticks.length.x.top = NULL, axis.ticks.length.x.bottom = NULL,
+  axis.ticks.length.y = NULL, axis.ticks.length.y.left = NULL,
+  axis.ticks.length.y.right = NULL, legend.box = NULL,
+  legend.key.size = unit(0,"pt"), legend.position = "none",
+  legend.text = element_blank(), legend.title = element_blank(),
+  strip.text = element_blank(), strip.switch.pad.grid = unit(0,"pt"),
+  strip.switch.pad.wrap = unit(0,"pt"), panel.ontop = FALSE,
+  panel.spacing = unit(5.5, "pt"), plot.margin = unit(c(0,0,0,0), "lines"),
+  plot.title = element_blank(), plot.subtitle = element_blank(),
+  plot.caption = element_blank(), plot.tag = element_blank(),
+  complete = TRUE, ...
+)
 
 # Main --------------------------------------------------------------------
-
-# grid <- seq(1,10,length.out=50) %>% 
-#   long_grid(.,.) %>% 
-#   mutate(noise = gen_perlin(x, y, seed = seed))
-# 
-# grid$angle <- curl_noise(gen_perlin, seed = seed, x = grid$x, y = grid$y) %>%
-#   { atan2(.$y, .$x) - atan2(grid$y, grid$x) }
-# 
-# grid %>%
-#   ggplot() +
-#   geom_regon(
-#     aes(x0 = x, y0 = y, r = noise/3, angle = angle, sides = 3, fill = noise), 
-#     alpha = 0.75
-#   ) +
-#   scale_fill_gradientn(
-#     colors = c("#ffc1c1", "#c6b6e0", "#c6b6e0", "#326983"), 
-#     guide = "none"
-#   ) +
-#   coord_fixed()
 
 points <- data.frame(merge(
     long_grid(seq(0,width,2*sqrt(3)*pr), seq(0,height,2*pr)),
@@ -108,12 +80,12 @@ points <- data.frame(merge(
   transmute(px = x+cos(px)*py, py = y+sin(px)*py)
 
 img <- genField(points, seed = seed, stepLength = stepLength, numSteps = numSteps) %>% 
-  ggplot(aes(x=x, y=y, group=line)) +
+  ggplot(aes(x=x, y=y, group=line, colour=as.factor(1))) +
   geom_path(lineend = "round") +
+  scale_colour_manual(values=c("white")) +
   coord_fixed() +
-  xlim(margin, width+margin) +
-  ylim(margin, height+margin) +
-  theme_void()
+  coord_cartesian(xlim = c(-margin,width+margin), ylim = c(-margin, height+margin)) +
+  (theme_void() %+replace% theme(panel.background = element_rect(fill="black"), legend.position = "none"))
 print("Finished plot")
 
 ggsave("test.png")
