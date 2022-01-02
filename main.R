4242f8a9aff2f267290f911e4b928dc892880c2a
Ike-G <07iglassbrook@brightoncollege.net>
2022-01-02 22:25:52 +0000

    finished bulk of algorithm

--- main.R
+++ main.R
@@ -8,6 +8,9 @@ library(particles)
 library(dplyr)
 library(vctrs)
 library(ptinpoly)
+library(ggnewscale)
+library(jcolors)
+library(paletteer)
 
 # Parameters --------------------------------------------------------------
 
@@ -18,7 +21,7 @@ pr <- 0.05
 ppc <- 8 
 width <- 1.5 
 height <- 1.5 
-margin <- 0 
+margin <- -0.1 
 innerMargin <- 0.1
 seed <- 127
 stepLength <- 0.01
@@ -85,30 +88,43 @@ intersect <- function(polygon, points) {
 
 filterPoly <- function(data, polygon, within = TRUE) {
   data <- data[order(data$line),,drop=FALSE]
+  numLines <- length(unique(data$line))
   inPoly <- ptinpoly::pip2d(polygon, as.matrix(data[c("x","y")]))
+  if (!within) {
+    inPoly <- -inPoly
+    print("Starting outer")
+  }
   inPoly[inPoly == 0] <- 1
   n <- nrow(data)
   diff <- inPoly[-1] != inPoly[-n] & data$line[-1] == data$line[-n]
-  
-  if (within) {
-    keep <- c(diff, FALSE) | c(FALSE, diff) | inPoly >= 0
-  } else {
-    keep <- c(diff, FALSE) | c(FALSE, diff) | inPoly <= 0
-  }
+  keep <- c(diff, FALSE) | c(FALSE, diff) | inPoly >= 0
   
   data <- data[keep,]
   inPoly <- inPoly[keep]
   n <- nrow(data)
   diff <- inPoly[-1] != inPoly[-n] & data$line[-1] == data$line[-n]
   
-  if (within) {
-    data[inPoly == -1,c("x","y")] <- purrr::map_dfr(
-      (1:(n-1))[diff], ~ intersect(polygon, data[.:(.+1),c("x","y")])
-    )  
-  } else {
-    data[inPoly == 1,c("x","y")] <- purrr::map_dfr(
-      (1:(n-1))[diff], ~ intersect(polygon, data[.:(.+1),c("x","y")])
-    )  
+  # j <- 0
+  # k <- 0
+  # for (i in (2:n+1)[c(diff, F) & c(F,diff)]) {
+  #   data[seq(i+j+2,n+1+j),] <- data[seq(i+j+1,n+j),]
+  #   j <- j+1
+  #   k <- k+j
+  # }
+  inPoly[c(diff, F) & c(F, diff)] <- 1
+  diff <- inPoly[-1] != inPoly[-n] & data$line[-1] == data$line[-n]
+  # For every i in the same group such that i+1 is different, replace it
+  # with the intersection between i and i+1
+  data[inPoly == -1,c("x","y")] <- purrr::map_dfr(
+    (1:(n-1))[diff], ~ intersect(polygon, data[.:(.+1),c("x","y")])
+  ) 
+  for (i in 1:numLines) {
+    s <- data$line == i
+    t <- purrr::accumulate(inPoly[s][-length(inPoly[s])],
+                                         function(a,b) ifelse(b == -1, a+1, a), 
+                                         .init = 0)
+    data[s, "line"] <- ifelse(t == 0, i, numLines+t)
+    numLines <- numLines+max(t)
   }
   data
 }
@@ -134,17 +150,22 @@ polygon <- c(rep(width/2,3),rep(height/2,3))+radius*
   matrix(c(1,-0.5,-0.5,0,sqrt(3)/2,-sqrt(3)/2),nrow=3,ncol=2)%*%
   matrix(c(cos(theta),-sin(theta),sin(theta),cos(theta)),nrow=2,ncol=2)
 
+lines <- genField(points, seed = seed, stepLength = stepLength, numSteps = numSteps)
+
 img <- ggplot() +
-  genField(points, seed = seed, stepLength = stepLength, numSteps = numSteps) %>% 
-  filterPoly(polygon) %>%
+  filterPoly(lines, polygon) %>%
   geom_path(mapping=aes(x=x, y=y, group=line, colour=c),lineend="round") +
-  # scale_colour_manual(values=c("white")) +
   scale_colour_distiller(palette="RdPu") +
-  genLines(centres, pr/2, seed) %>%
-  filterPoly(polygon, within = FALSE) %>%
-  geom_path(mapping=aes(x=x,y=y,group=line),lineend="round") +
+  # jcolors::scale_color_jcolors_contin("pal2") +
+  new_scale_color() +
+  filterPoly(lines, polygon, within = FALSE) %>%
+  geom_path(mapping=aes(x=x,y=y,group=line, colour=c),lineend="round") +
+#  jcolors::scale_color_jcolors_contin("pal3") +
+  paletteer::scale_color_paletteer_c("scico::oslo") +
+  #scale_colour_distiller(palette = "Greys") +
+  #geom_path(tibble(x=polygon[c(1:3,1:2),1],y=polygon[c(1:3,1:2),2]), mapping=aes(x=x,y=y), size=2, linejoin="mitre", lineend="round") + 
   coord_cartesian(xlim = c(-margin,width+margin), ylim = c(-margin, height+margin)) +
-  (theme_void() %+replace% theme(legend.position = "none"))
+  (theme_void() %+replace% theme(legend.position = "none", panel.background = element_rect(fill = "black")))
 
 print("Finished plot")
 
